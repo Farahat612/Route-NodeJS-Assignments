127d0e5c6545a0d37ac7cd5e110edf6ee9ba4e98
Mohamed Farahat <mohamed_30863052@f-eng.tanta.edu.eg>
2024-07-19 18:27:41 +0300

    books routes

--- Assignment08/index.js
+++ Assignment08/index.js
@@ -3,6 +3,8 @@ import express from 'express'
 
 import connectDB from './config/db.js'
 
+import bookRoutes from './routes/bookRoutes.js'
+
 // Load env vars
 dotenv.config()
 // Connect to database
@@ -11,6 +13,9 @@ connectDB()
 const app = express()
 // Middleware
 app.use(express.json())
+
+// Routes
+app.use('/api/books', bookRoutes)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
